85488169a333b6f6266397d0d5d056782951d2a0
gchakra3 <chakraborty.gourab22@gmail.com>
2025-07-04 23:38:42 +0530

    Weekly Class Scheduling Module

--- src/features/admin/index.ts
+++ src/features/admin/index.ts
@@ -9,10 +9,11 @@ export { default as FormSubmissions } from './components/FormSubmissions.tsx';
 export { default as InstructorManagement } from './components/InstructorManagement.tsx';
 export { default as NewsletterManagement } from './components/NewsletterManagement.tsx';
 export { default as UserRoleManagement } from './components/UserRoleManagement.tsx';
+export { default as WeeklyClassScheduler } from './components/WeeklyClassScheduler.tsx';
+export { default as ClassTypeManager } from './components/ClassTypeManager.tsx';
 
 // Contexts
 export { default as AdminContext } from './contexts/AdminContext.tsx';
 
 // Pages
 export { default as AdminDashboard } from './pages/AdminDashboard.tsx';
-
